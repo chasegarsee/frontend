5f083006b17198f048081269341a657ec2e2a964
jeffyjkang <jeffyjkang@gmail.com>
2019-03-12 12:08:05 -0700

    update to hoc

--- src/hocComponents/register.js
+++ src/hocComponents/register.js
@@ -8,8 +8,46 @@ class Register extends Component {
       passwordInput: ""
     };
   }
+  editInputHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  submitHandler = e => {
+    e.preventDefault();
+    const username = this.state.usernameInput;
+    const password = this.state.passwordInput;
+    localStorage.setItem("user", username);
+    localStorage.setItem("pass", password);
+    this.setState({
+      usernameInput: "",
+      passwordInput: ""
+    });
+  };
   render() {
-    return <div>placeholder</div>;
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <div>username:</div>
+          <input
+            name="usernameInput"
+            type="text"
+            onChange={this.editInputHandler}
+            placeholder="Username"
+            value={this.state.usernameInput}
+            required
+          />
+          <div>password:</div>
+          <input
+            name="passwordInput"
+            type="password"
+            onChange={this.editInputHandler}
+            placeholder="Password"
+            value={this.state.passwordInput}
+            required
+          />
+          <button onClick={this.submitHandler}>register</button>
+        </form>
+      </div>
+    );
   }
 }
 
